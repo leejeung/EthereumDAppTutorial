0af8b78c294088355723746946b665e31bab609b
leejeung <leejeung@grinnell.edu>
2018-07-11 20:05:22 +0900

    Cast vote

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,6 +16,11 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
     // Constructor
     function Election () public {
         addCandidate("Candidate 1");
@@ -39,5 +44,8 @@ contract Election {
 
         // update candidate vote Count
         candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
     }
 }
